1881a21b2d0788833cb4f558225b56c3490d5b8d
nqtuan204 <56553265+nqtuan204@users.noreply.github.com>
2022-04-15 20:26:18 +0700

    Add project files.

---
+++ ElectronicComponentsShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElectronicComponentsShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
